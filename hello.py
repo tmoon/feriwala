f364db9e7320e676d710f9d000d3e95ea4cb80da
Tarik Adnan Moon <tmoon@college.harvard.edu>
2014-08-17 12:26:31 -0400

    change hello

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from random import random
 hello = Flask(__name__)
 
 @hello.route('/')
 def hello_world():
-    return 'Hello World! New site!'
+    return 'Hello World! New site!' + str( random() )
 
 if __name__ == '__main__':
     hello.run()
